361aa69c6128dab4beba5b112fee4553dc36c676
Antonius979 <antoniusindra979@gmail.com>
2022-12-01 14:43:21 +0700

    first commit

---
+++ app/src/main/java/com/anton/gd8_c_0542/models/Mahasiswa.kt
@@ -0,0 +1,9 @@
+package com.anton.gd8_c_0542.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anton.gd8_c_0542.R
+
+class Mahasiswa (var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+  var id: Long? = null
+}
